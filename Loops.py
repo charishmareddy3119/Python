f83b7e10dce06a5dc4fe4bf31f71a31cac8fda24
charishmareddy3119 <kcharishma3101@gmail.com>
2026-09-19 15:07:52 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,42 @@
+for in range(100):
+    print("Hello")
+
+    s = [1,2,3,4,5]
+    for x in s:
+        print(x)
+
+ for i in range(1,11,2):
+   print(i)
+
+ for z in range(50):
+    print(z)
+
+ for k in range(0,11):
+    print(k)
+
+  for s in (str("queen")):
+     print(s)
+
+ v=[3,5,7,8]
+ for s in v:
+    print(s)
+
+ t=(7,8,6,4)
+for v in t:
+    print(v)
+
+ u={3,9,4,5}
+ for v in u:
+   print(v)
+
+   s = 'animal'
+   s=input("Enter any name:")
+   x = 0
+
+ for i in s:
+   print("The index of",i,"is:",x)
+x = x+1
+
+# Find the sum of any 5 products
+
+ 
