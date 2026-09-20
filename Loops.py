bfd9c3d57b60a846caca5079b0fe6479328a753a
charishmareddy3119 <kcharishma3101@gmail.com>
2026-09-20 15:35:46 +0530

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -1,42 +1,124 @@
-for in range(100):
-    print("Hello")
+  #for in range(100):
+    #print("Hello")
 
-    s = [1,2,3,4,5]
-    for x in s:
-        print(x)
+    #s = [1,2,3,4,5]
+    #for x in s:
+       # print(x)
 
- for i in range(1,11,2):
-   print(i)
+ #for i in range(1,11,2):
+   #print(i)
 
- for z in range(50):
-    print(z)
+ #for z in range(50):
+  #print(z)
 
- for k in range(0,11):
-    print(k)
+ #for k in range(0,11):
+    #print(k)
 
-  for s in (str("queen")):
-     print(s)
+  #for s in (str("queen")):
+    #print(s)
 
- v=[3,5,7,8]
- for s in v:
-    print(s)
+ #v=[3,5,7,8]
+ #for s in v:
+    #print(s)
 
- t=(7,8,6,4)
-for v in t:
-    print(v)
+ #t=(7,8,6,4)
+#for v in t:
+    #print(v)
 
- u={3,9,4,5}
- for v in u:
-   print(v)
+ #u={3,9,4,5}
+ #for v in u:
+   #print(v)
 
-   s = 'animal'
-   s=input("Enter any name:")
-   x = 0
+   #s = 'animal'
+   #s=input("Enter any name:")
+   #x = 0
 
- for i in s:
-   print("The index of",i,"is:",x)
-x = x+1
+ #for i in s:
+   #print("The index of",i,"is:",x)
+#x = x+1
 
 # Find the sum of any 5 products
 
- 
+#
+#a=10
+#b=20
+#c=30
+#d=40
+#e=50
+#sum=a+b+c+d+e
+#print(sum)
+
+ #for i in range(1,20):
+  # print(i)
+
+#a = [10, 20, 5, 25, 15]
+#max = 0
+
+#for i in a:
+    #if i > max:
+        #max = i
+
+#print(max)
+
+#a = "hello"
+#count = 0
+
+#for i in a:
+   #if i in "aeiou":
+        #count = count + 1
+
+#print(count)
+
+#a = [1, 2, 3, 4]
+#b = [3, 4, 5, 6]
+
+#for i in a:
+    #if i in b:
+        #print(i)
+
+#a = "Charishma"
+
+#for i in a[::-1]:
+    #print(i)
+
+#a = [10, 20, 60 , 100, 50]
+#sum = 0
+
+#for i in a:
+    #sum = sum + i
+
+#print(sum)
+
+#a = "My name is Cherry"
+#count = 5
+
+#for i in a.split():
+    #count = count + 1
+
+#print(count)
+
+#for i in range(2, 30):
+    #count = 0
+
+    #for j in range(1, i + 1):
+        #if i % j == 0:
+            #count = count + 1
+
+    #if count == 2:
+        #print(i)
+
+#n = 20
+#sum = 0
+
+#for i in range(1, n + 1):
+    #sum = sum + i
+
+#print(sum)
+
+#n = 10
+#fact = 1
+
+#for i in range(1, n + 1, 1):
+    #fact = fact * i
+
+#print(fact)
