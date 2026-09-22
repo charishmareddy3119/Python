0d9e2933dabde32ccdf04cdd8dc5680a5443525c
charishmareddy3119 <kcharishma3101@gmail.com>
2026-09-22 15:03:07 +0530

    Add files via upload

---
+++ Cherry.py
@@ -0,0 +1,53 @@
+#a='cherry'
+#if 'c' in a:
+    #print("True")
+#else:
+    #print("False")
+
+#a=input("print main string:")
+#b=input("Enter substring:")
+#if b in a:
+    #print("Substring prsent in mainstring")
+#else:
+    #print("Not present")    
+
+# a = '0'
+# print(a)
+# print(type(a))
+
+# b = 'Java is programming language'
+# print(b)
+# print(b[14])
+
+# s = '      Python'
+# print(s) 
+# print(s.strip())
+
+# a = 'learning python is boring language'
+# print(len(a))
+# print(a.lower())
+# print(a.upper())
+# print(a.swapcase())
+# print(a.capitalize())
+
+# x = ("Imagine a college where every classroom is filled with creativity,"
+#      " every corridor has a new adventure, and every student discovers something"
+#      " special about themselves. This magical college is not just a place to study;"
+#      " it is a place where dreams come alive."
+#      " In this college, books can tell stories, laboratories can create amazing inventions,"
+#      " and teachers inspire students to explore new ideas. The campus is surrounded by beautiful"
+#      " gardens, glowing lights, and peaceful spaces where students can learn and relax.")
+
+# print(x.count("i"))
+# print(a.split(100))
+
+
+# a = 'python is very '\
+#     'difficult to understand'
+# print(a.replace("difficult",'easy'))
+# print(a.startswith("python"))
+# print(a.startswith("Java"))
+# print(a.endswith("Java"))
+# print(a.endswith("understand"))
+# print(a.split())
+
